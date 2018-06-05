88e4d0a399819d7dc60f85a949e861fcbfbe996d
Elvinn <elvinn.tombert@hetic.net>
2018-06-05 14:09:15 +0200

    lol

--- app/src/main/java/com/example/elvinntombert/myapplication/ProjectActivity.kt
+++ app/src/main/java/com/example/elvinntombert/myapplication/ProjectActivity.kt
@@ -1,12 +1,19 @@
 package com.example.elvinntombert.myapplication
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_project.*
 
 class ProjectActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_project)
+
+    button.setOnClickListener{
+      var intent = Intent(this,Home::class.java)
+      startActivity(intent)
+    }
   }
 }
