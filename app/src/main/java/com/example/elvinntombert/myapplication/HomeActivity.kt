d0ed78f94eaf32af447894e41fd22a3bda988fee
Artikodin <artium.wasse@gmail.com>
2018-06-07 18:27:03 +0200

    tap init ok

--- app/src/main/java/com/example/elvinntombert/myapplication/HomeActivity.kt
+++ app/src/main/java/com/example/elvinntombert/myapplication/HomeActivity.kt
@@ -2,10 +2,6 @@ package com.example.elvinntombert.myapplication
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v7.widget.LinearLayoutManager
-import com.example.elvinntombert.myapplication.item.EventItem
-import com.example.elvinntombert.myapplication.model.Event
-import com.mikepenz.fastadapter.commons.adapters.FastItemAdapter
 import kotlinx.android.synthetic.main.activity_home.*
 
 
@@ -14,27 +10,12 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        val events = arrayListOf<Event>()
-        events.add(Event("Lorem ipsum dolor sit amlet", "Rue de Rivoli, 75001 Paris","22 juin - 9h30", "25€","1"))
-        events.add(Event("Lorem ipsum dolor sit amlet", "Rue de Rivoli, 75001 Paris","22 juin - 9h30", "25€","2"))
-        events.add(Event("Lorem ipsum dolor sit amlet", "Rue de Rivoli, 75001 Paris","22 juin - 9h30", "25€","3"))
-        events.add(Event("Lorem ipsum dolor sit amlet", "Rue de Rivoli, 75001 Paris","22 juin - 9h30", "25€","4"))
+        val pagerAdapter = PageAdapter(supportFragmentManager, 4)
 
-        var linearLayoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        homeViewPager.adapter = pagerAdapter
 
-        eventRecyclerView.layoutManager = linearLayoutManager
-        val adapter = FastItemAdapter<EventItem>()
+        mainTabLayout.setupWithViewPager(homeViewPager)
 
-        adapter.add(events.map{ EventItem(it)})
 
-        // On lie le recyclerView à l'adaptateur
-        eventRecyclerView.adapter = adapter
-
-        adapter.withOnClickListener{v, adapter, item, position ->
-            val event = item.event
-            val intent = EventDetailActivity.newIntent(this, event)
-            startActivity(intent)
-            true // on a consommé le tap sur le cellule
-        }
     }
 }
