485b99c1eefb8e45d853a35054220d0b2720ae0a
Artikodin <artium.wasse@gmail.com>
2018-06-07 18:27:03 +0200

    tap init ok

--- app/src/main/java/com/example/elvinntombert/myapplication/HomeActivity.kt
+++ app/src/main/java/com/example/elvinntombert/myapplication/HomeActivity.kt
@@ -2,10 +2,6 @@ package com.example.elvinntombert.myapplication
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v7.widget.LinearLayoutManager
-import com.example.elvinntombert.myapplication.item.EventItem
-import com.example.elvinntombert.myapplication.model.Event
-import com.mikepenz.fastadapter.commons.adapters.FastItemAdapter
 import kotlinx.android.synthetic.main.activity_home.*
 
 
@@ -14,27 +10,12 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        val events = arrayListOf<Event>()
-        events.add(Event("Le Louvre", "Rue de Rivoli, 75001 Paris","22 juin - 9h30", "25€", "Tourisme","1"))
-        events.add(Event("Disneyland Paris 25ème anniversaire", "Rue de Ravoli, 75001 Paris","21 juin - 10h30", "30€", "Loisir","2"))
-        events.add(Event("Parc Astérix", "Rue de Ruvoli, 75001 Paris","23 juin - 11h30", "50€", "Loisir","3"))
-        events.add(Event("Un test au pif", "Rue de Rovoli, 75001 Paris","20 juin - 12h30", "40€", "Test","4"))
+        val pagerAdapter = PageAdapter(supportFragmentManager, 4)
 
-        var linearLayoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        homeViewPager.adapter = pagerAdapter
 
-        eventRecyclerView.layoutManager = linearLayoutManager
-        val adapter = FastItemAdapter<EventItem>()
+        mainTabLayout.setupWithViewPager(homeViewPager)
 
-        adapter.add(events.map{ EventItem(it)})
 
-        // On lie le recyclerView à l'adaptateur
-        eventRecyclerView.adapter = adapter
-
-        adapter.withOnClickListener{v, adapter, item, position ->
-            val event = item.event
-            val intent = EventDetailActivity.newIntent(this, event)
-            startActivity(intent)
-            true // on a consommé le tap sur le cellule
-        }
     }
 }
---
+++ app/src/main/java/com/example/elvinntombert/myapplication/TabHome.kt
@@ -0,0 +1,66 @@
+package com.example.elvinntombert.myapplication
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.elvinntombert.myapplication.item.EventItem
+import com.example.elvinntombert.myapplication.model.Event
+import com.mikepenz.fastadapter.commons.adapters.FastItemAdapter
+import kotlinx.android.synthetic.main.fragment_tab_home.view.*
+
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ *
+ */
+class TabHome : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_tab_home, container, false)
+
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val events = arrayListOf<Event>()
+        events.add(Event("Le Louvre", "Rue de Rivoli, 75001 Paris","22 juin - 9h30", "25€", "Tourisme","1"))
+        events.add(Event("Disneyland Paris 25ème anniversaire", "Rue de Ravoli, 75001 Paris","21 juin - 10h30", "30€", "Loisir","2"))
+        events.add(Event("Parc Astérix", "Rue de Ruvoli, 75001 Paris","23 juin - 11h30", "50€", "Loisir","3"))
+        events.add(Event("Un test au pif", "Rue de Rovoli, 75001 Paris","20 juin - 12h30", "40€", "Test","4"))
+
+        var linearLayoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+
+
+        view.eventRecyclerView.layoutManager = linearLayoutManager
+        val adapter = FastItemAdapter<EventItem>()
+
+        adapter.add(events.map{ EventItem(it) })
+
+        // On lie le recyclerView à l'adaptateur
+        view.eventRecyclerView.adapter = adapter
+
+        adapter.withOnClickListener{v, adapter, item, position ->
+            val event = item.event
+            val intent = context?.let { EventDetailActivity.newIntent(it, event) }
+            startActivity(intent)
+//            if(context != null){
+//            }
+            true // on a consommé le tap sur le cellule
+        }
+    }
+
+
+}
